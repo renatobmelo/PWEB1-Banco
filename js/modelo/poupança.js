f7a78324fb7c9101751e72f65ee9f4bdc7eb38c5
Renato Melo <renato.bmelo@outlook.com>
2023-09-03 17:50:41 -0300

    classe criada e html mexido

---
+++ js/modelo/poupança.js
@@ -0,0 +1,11 @@
+class Poupanca extends Conta {
+    constructor(numero, saldo,dataAniversario) {
+        super(numero, saldo);
+        this.dataAniversario = dataAniversario;
+    }
+
+    creditar(valor) {
+        super.creditar(valor * 1.1);
+    }
+
+}
